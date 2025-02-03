d19423f2cf9dff3eb0acebc76d798132cd7c651d
david <mcgluedavid@gmail.com>
2025-02-03 21:39:05 +0000

    second commit

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 
 
-rm fol_1
-rm fol_2
-
+rm -R fol_1
+rm -R fol_2
 mkdir fol_1 
 mkdir fol_2
 
@@ -21,3 +20,17 @@ touch .gitignore
 echo ".gitignore" >> .gitignore
 echo fol_1 >> .gitignore
 echo fol_2 >> .gitignore
+
+chmod 600 fol_1/1_1.txt
+chmod 600 fol_1/1_3.txt
+chmod 600 fol_2/2_1.txt
+chmod 600 fol_2/2_3.txt
+chmod 777 fol_1/1_2.txt
+chmod 777 fol_1/1_2.txt
+cd ..
+touch README.md
+echo "# # week 2 Lab – Bash & Git" > README.md
+echo david mcglue 21147141 >> README.md
+echo "[lab repo] (https://github.com/barkingdavetron/devops-lab-2-task)" >> README.md
+echo --- >> README.md
+echo "To run script.sh run: \`bash script.sh\`" >> README.md
